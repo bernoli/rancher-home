54757b7d64b5c6a660134106fe303d5ebde741f3
Alessandro Vozza <alessandro.vozza@microsoft.com>
2020-06-08 17:39:53 +0200

    end }

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -1,102 +1,103 @@
 resource "rke_cluster" "cluster" {
 
-cluster_name =  "rancher-home"
-ssh_key_path = "/home/alessandro/.ssh/id_rsa"
+  cluster_name = "rancher-home"
+  ssh_key_path = "/home/alessandro/.ssh/id_rsa"
 
-ignore_docker_version = true
+  ignore_docker_version = true
 
 
-network {
+  network {
     plugin = "canal"
-}
+  }
 
-ingress {
+  ingress {
     provider = "none"
-}
+  }
 
-services {
+  services {
     etcd-snapshot {
-        retention = "3d"
-        interval_hours = "1h"
+      retention      = "3d"
+      interval_hours = "1h"
     }
 
     kube-controller {
-        extra_args = {
-            feature-gates = "EphemeralContainers=true"
-        }
+      extra_args = {
+        feature-gates = "EphemeralContainers=true"
+      }
     }
 
     scheduler {
-        extra_args = {
-            feature-gates = "EphemeralContainers=true"
-        }
+      extra_args = {
+        feature-gates = "EphemeralContainers=true"
+      }
     }
 
     kubelet {
-        max-pods = 250
-        extra_args = {
-            feature-gates = "EphemeralContainers=true"
-        }
+      max-pods = 250
+      extra_args = {
+        feature-gates = "EphemeralContainers=true"
+      }
     }
 
     kube-api {
-        secrets_encryption_config {
-              enabled = true
-          }
-                extra_args = {
-            feature-gates = "EphemeralContainers=true"
-            encryption-provider-config = "/etc/kubernetes/encryption.yaml"
-            oidc-client-id = "spn:5bd13b05-c17a-4589-8a61-f2185ab7831f"
-            oidc-issuer-url = "https://sts.windows.net/a68cdba5-68a4-49a3-8a42-2823316db54f/"
-            oidc-username-claim = "oid"
-            oidc-groups-claim = "groups"
-        }
+      secrets_encryption_config {
+        enabled = true
+      }
+      extra_args = {
+        feature-gates              = "EphemeralContainers=true"
+        encryption-provider-config = "/etc/kubernetes/encryption.yaml"
+        oidc-client-id             = "spn:5bd13b05-c17a-4589-8a61-f2185ab7831f"
+        oidc-issuer-url            = "https://sts.windows.net/a68cdba5-68a4-49a3-8a42-2823316db54f/"
+        oidc-username-claim        = "oid"
+        oidc-groups-claim          = "groups"
+      }
     }
-}
+  }
 
   nodes {
-    address      = "cloud.stackmasters.com"
+    address          = "cloud.stackmasters.com"
     internal_address = "192.168.178.2"
-    user         = "alessandro"
-    role         = ["controlplane", "etcd","worker"]
-    ssh_key_path = "~/.ssh/id_rsa"
+    user             = "alessandro"
+    role             = ["controlplane", "etcd", "worker"]
+    ssh_key_path     = "~/.ssh/id_rsa"
 
     labels = {
-      node-role.kubernetes.io/master =  ""
+      node-role.kubernetes.io / master = ""
     }
   }
 
   nodes {
-    address      = "nuc1"
+    address          = "nuc1"
     internal_address = "192.168.178.5"
-    user         = "alessandro"
-    role         = ["worker"]
-    ssh_key_path = "~/.ssh/id_rsa"
+    user             = "alessandro"
+    role             = ["worker"]
+    ssh_key_path     = "~/.ssh/id_rsa"
   }
 
   nodes {
-    address      = "nuc2"
+    address          = "nuc2"
     internal_address = "192.168.178.6"
-    user         = "alessandro"
-    role         = ["worker"]
-    ssh_key_path = "~/.ssh/id_rsa"
+    user             = "alessandro"
+    role             = ["worker"]
+    ssh_key_path     = "~/.ssh/id_rsa"
   }
 
   nodes {
-    address      = "nuc1"
+    address          = "nuc1"
     internal_address = "192.168.178.9"
-    user         = "alessandro"
-    role         = ["worker"]
-    ssh_key_path = "~/.ssh/id_rsa"
+    user             = "alessandro"
+    role             = ["worker"]
+    ssh_key_path     = "~/.ssh/id_rsa"
   }
 
-authentication {
+  authentication {
     strategy = "x509"
     sans = [
-        "nas.stackmasters.com"
+      "nas.stackmasters.com"
     ]
-}
+  }
 
-system_images {
+  system_images {
     kubernetes = ["rancher/hyperkube:v1.18.3-rancher1"]
+  }
 }
